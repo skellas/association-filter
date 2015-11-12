bb72d3a46ce002e40b737add2e9483f29f2b4b3e
Seth Kellas <seth.kellas@ixxus.com>
2015-11-12 11:26:11 -0500

    Creation of Clean Slate. Time to start brainstorming in here.

---
+++ run.bat
@@ -0,0 +1,16 @@
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+::      Dev environment startup script for Alfresco Community     ::
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+@echo off
+
+set springloadedfile=%HOME%\.m2\repository\org\springframework\springloaded\1.2.3.RELEASE\springloaded-1.2.3.RELEASE.jar
+
+if not exist %springloadedfile% (
+  mvn validate -Penterprise -Psetup
+)
+
+set MAVEN_OPTS=-javaagent:"%springloadedfile%" -noverify -Xms256m -Xmx1G
+
+::cd runner && mvn clean  && cd .. && mvnDebug install -Penterprise -Prun -nsu
+mvn install -Penterprise -Prun -nsu
+:: mvn install -Penterprise -Prun 
